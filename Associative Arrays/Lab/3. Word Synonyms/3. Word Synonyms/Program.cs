2fdfc2db900360dfb7a1ac50642b68bb56dd3e22
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-04-18 15:15:34 +0300

    Create a program, which keeps a dictionary with synonyms. The key of the dictionary will be the word. The value will be a list of all the synonyms of that word. You will be given a number n – the count of the words. After each word, you will be given a synonym, so the count of lines you have to read from the console is 2 * n. You will be receiving a word and a synonym each on a separate line like this: •	{word} •	{synonym} If you get the same word twice, just add the new synonym to the list. Print the words in the following format: "{word} - {synonym1, synonym2… synonymN}"

---
+++ Associative Arrays/Lab/3. Word Synonyms/3. Word Synonyms/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._Word_Synonyms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
+            bool synonym = false;
+            string key = "";
+            for (int i = 0; i < n * 2; i++)
+            {
+                string word = Console.ReadLine();
+                if (!words.ContainsKey(word) && !synonym)
+                {
+                    words.Add(word, new List<string>());
+                    synonym = true;
+                    key = word;
+                }
+                else if (words.ContainsKey(word) && !synonym)
+                {
+                    key = word;
+                    synonym = true;
+                }
+                else if (synonym)
+                {
+                    words[key].Add(word);
+                    synonym = false;
+                }
+            }
+            foreach (var word in words)
+            {
+                Console.Write($"{word.Key} - {string.Join(", ", word.Value)}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
