26691fcc07bdd76ff15a01235974f08fda2ca826
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-04-20 11:52:48 +0300

    Read an array of strings and take only words, whose length is even. Print each word on a new line.

---
+++ Associative Arrays/Lab/5. Word Filter/5. Word Filter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _5._Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+            string[] sortedWords = words.Where(n => n.Length % 2 == 0).ToArray();
+            foreach (var item in sortedWords)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
