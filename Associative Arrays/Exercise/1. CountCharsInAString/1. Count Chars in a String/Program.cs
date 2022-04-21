cf310daa740b39d1e5621cc7d6aaf1534f31c10b
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-04-21 19:58:46 +0300

    Create a program that counts all characters in a string except for space (' '). Print all the occurrences in the following format: "{char} -> {occurrences}"

---
+++ Associative Arrays/Exercise/1. CountCharsInAString/1. Count Chars in a String/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().ToArray();
+            var count = new Dictionary<char, int>();
+            foreach (string word in words)
+            {
+                foreach (char item in word)
+                {
+                    if (!count.ContainsKey(item))
+                    {
+                        count.Add(item, 1);
+                    }
+                    else
+                    {
+                        count[item]++;
+                    }
+                }
+            }
+            foreach (var sorted in count)
+            {
+                Console.WriteLine($"{sorted.Key} -> {sorted.Value}");
+            }
+        }
+    }
+}
