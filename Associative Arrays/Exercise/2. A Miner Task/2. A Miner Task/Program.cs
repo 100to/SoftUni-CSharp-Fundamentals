39b7af56276edeb977e63db2ac9a7d08681018a4
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-04-22 20:45:56 +0300

    You will be given a sequence of strings, each on a new line. Every odd line on the console is representing a resource (e.g. Gold, Silver, Copper, and so on) and every even - quantity. Your task is to collect the resources and print them each on a new line. Print the resources and their quantities in the following format: "{resource} –> {quantity}"

---
+++ Associative Arrays/Exercise/2. A Miner Task/2. A Miner Task/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2._A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resource = string.Empty;
+            int quantity = 0;
+            var minerCollection = new Dictionary<string, int>();
+            while (true)
+            {
+                resource = Console.ReadLine();
+                if (resource == "stop")
+                {
+                    break;
+                }
+                quantity = int.Parse(Console.ReadLine());
+                if (minerCollection.ContainsKey(resource))
+                {
+                    minerCollection[resource] += quantity;
+                }
+                else
+                {
+                    minerCollection.Add(resource, quantity);
+                }
+            }
+            foreach (var sorted in minerCollection)
+            {
+                Console.WriteLine($"{sorted.Key} -> {sorted.Value}");
+            }
+        }
+    }
+}
