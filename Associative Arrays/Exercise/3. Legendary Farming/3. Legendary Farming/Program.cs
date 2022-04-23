244643c316d0f456f7e4ad92670b05209e7b8ff0
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-04-23 11:37:49 +0300

    You’ve done all the work and the last thing left to accomplish is to own a legendary item. However, it's a tedious process and it requires quite a bit of farming. Anyway, you are not too pretentious - any legendary item will do. The possible items are: •	Shadowmourne - requires 250 Shards; •	Valanyr - requires 250 Fragments; •	Dragonwrath - requires 250 Motes; Shards, Fragments and Motes are the key materials and everything else is junk. You will be given lines of input, in the format: "2 motes 3 ores 15 stones" Keep track of the key materials - the first one that reaches the 250 marks, wins the race. At that point, you have to print that the corresponding legendary item is obtained. Then, print the remaining shards, fragments, motes, ordered by quantity in descending order, then by name in ascending order, each on a new line. Finally, print the collected junk items in alphabetical order.

---
+++ Associative Arrays/Exercise/3. Legendary Farming/3. Legendary Farming/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Legendary_Farming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var minerCollection = new Dictionary<string, int>();
+            var junk = new Dictionary<string, int>();
+            minerCollection.Add("shards", 0);
+            minerCollection.Add("motes", 0);
+            minerCollection.Add("fragments", 0);
+            bool obtained = false;
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                for (int i = 0; i < input.Length - 1; i += 2)
+                {
+                    int quantity = int.Parse(input[i]);
+                    string resource = input[i + 1].ToLower();
+                    if (minerCollection.ContainsKey(resource))
+                    {
+                        minerCollection[resource] += quantity;
+                    }
+                    else if (junk.ContainsKey(resource))
+                    {
+                        junk[resource] += quantity;
+                    }
+                    else
+                    {
+                        junk.Add(resource, quantity);
+                    }
+                    if (minerCollection["shards"] >= 250)
+                    {
+                        Console.WriteLine("Shadowmourne obtained!");
+                        minerCollection["shards"] -= 250;
+                        obtained = true;
+                        break;
+                    }
+                    else if (minerCollection["fragments"] >= 250)
+                    {
+                        Console.WriteLine("Valanyr obtained!");
+                        minerCollection["fragments"] -= 250;
+                        obtained = true;
+                        break;
+                    }
+                    else if (minerCollection["motes"] >= 250)
+                    {
+                        Console.WriteLine("Dragonwrath obtained!");
+                        minerCollection["motes"] -= 250;
+                        obtained = true;
+                        break;
+                    }
+                }
+                if (obtained)
+                {
+                    break;
+                }
+            }
+            foreach (var item in minerCollection)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+            foreach (var item in junk)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
