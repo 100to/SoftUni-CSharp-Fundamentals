5e449afc8a30e4e57a62b3262a1da54cc354acbc
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-04-24 18:07:37 +0300

    Create a program that keeps the information about products and their prices. Each product has a name, a price, and a quantity. If the product doesn’t exist yet, add it with its starting quantity. If you receive a product, which already exists, increases its quantity by the input quantity and if its price is different, replace the price as well. You will receive products’ names, prices, and quantities on new lines. Until you receive the command "buy", keep adding items. When you do receive the command "buy", print the items with their names and the total price of all the products with that name.

---
+++ Associative Arrays/Exercise/4. Orders/4. Orders/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var productList = new Dictionary<string, List<decimal>>();
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                string key = input[0];
+                if (key == "buy")
+                {
+                    break;
+                }
+                decimal price = decimal.Parse(input[1]);
+                decimal quantity = decimal.Parse(input[2]);
+                if (!productList.ContainsKey(key))
+                {
+                    productList.Add(key, new List<decimal>() { price, quantity });
+                }
+                else
+                {
+                    productList[key][0] = price;
+                    productList[key][1] += quantity;
+                }
+            }
+            foreach (var item in productList)
+            {
+                decimal sum = item.Value[0] * item.Value[1];
+                Console.WriteLine($"{item.Key} -> {sum:f2}");
+            }
+        }
+    }
+}
