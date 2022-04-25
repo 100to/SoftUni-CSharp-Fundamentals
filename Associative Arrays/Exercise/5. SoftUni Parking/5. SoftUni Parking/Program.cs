7ade4ba89da8387c3a02d2e61f54db9bd3ae132a
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-04-25 11:38:22 +0300

    SoftUni just got a new parking lot. It’s so fancy, it even has online parking validation. Except the online service doesn’t work. It can only receive users’ data, but it doesn’t know what to do with it. Good thing you’re on the dev team and know how to fix it, right? Write a program, which validates a parking place for an online service. Users can register to park and unregister to leave. The program receives 2 commands: •	"register {username} {licensePlateNumber}": o	The system only supports one car per user at the moment, so if a user tries to register another license plate, using the same username, the system should print: "ERROR: already registered with plate number {licensePlateNumber}" o	If the aforementioned checks passes successfully, the plate can be registered, so the system should print:  "{username} registered {licensePlateNumber} successfully" •	"unregister {username}": o	If the user is not present in the database, the system should print: "ERROR: user {username} not found" o	If the aforementioned check passes successfully, the system should print: "{username} unregistered successfully" After you execute all of the commands, print all the currently registered users and their license plates in the format: •	"{username} => {licensePlateNumber}"

---
+++ Associative Arrays/Exercise/5. SoftUni Parking/5. SoftUni Parking/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._SoftUni_Parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var parkingLot = new Dictionary<string, string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                string command = input[0];
+                string key = input[1];
+                if (command == "register")
+                {
+                    string license = input[2];
+                    if (!parkingLot.ContainsKey(key))
+                    {
+                        parkingLot.Add(key, license);
+                        Console.WriteLine($"{key} registered {license} successfully");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {license}");
+                    }
+                }
+                else if (command == "unregister")
+                {
+                    if (!parkingLot.ContainsKey(key))
+                    {
+                        Console.WriteLine($"ERROR: user {key} not found");
+                    }
+                    else
+                    {
+                        parkingLot.Remove(key);
+                        Console.WriteLine($"{key} unregistered successfully");
+                    }
+                }
+            }
+            foreach (var item in parkingLot)
+            {
+                Console.WriteLine($"{item.Key} => {item.Value}");
+            }
+        }
+    }
+}
