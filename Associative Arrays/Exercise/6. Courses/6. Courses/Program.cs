dd4fa1c8e47daa9565f798e289ef829199b05631
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-04-26 23:01:19 +0300

    Create a program that keeps the information about courses. Each course has a name and registered students. You will be receiving a course name and a student name until you receive the command "end". Check if such a course already exists, and if not, add the course. Register the user into the course. When you receive the command "end", print the courses with their names and total registered users, ordered by the count of registered users in descending order. For each contest print, the registered users ordered by name in ascending order.

---
+++ Associative Arrays/Exercise/6. Courses/6. Courses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _6._Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coursePeople = new Dictionary<string, List<string>>();
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(" : ").ToArray();
+                if (input[0] == "end")
+                {
+                    break;
+                }
+                string course = input[0];
+                string name = input[1];
+                if (!coursePeople.ContainsKey(course))
+                {
+                    coursePeople.Add(course, new List<string>() { name });
+                }
+                else
+                {
+                    coursePeople[course].Add(name);
+                }
+            }
+            foreach (var item in coursePeople)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value.Count}");
+                foreach (var item2 in item.Value)
+                {
+                    Console.WriteLine($"-- {item2}");
+                }
+            }
+        }
+    }
+}
