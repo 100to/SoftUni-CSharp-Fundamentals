4aacc5a5a0f755b6c593cfa32f4e20646f09870a
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-04-27 09:27:02 +0300

    Create a program that keeps the information about students and their grades. You will receive n pair of rows. First, you will receive the student's name, after that, you will receive his grade. Check if the student already exists and if not, add him. Keep track of all grades for each student. When you finish reading the data, keep the students with an average grade higher than or equal to 4.50. Order the filtered students by average grade in descending order. Print the students and their average grade in the following format: "{name} –> {averageGrade}" Format the average grade to the 2nd decimal place.

---
+++ Associative Arrays/Exercise/7.  Student Academy/7.  Student Academy/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7.__Student_Academy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var studentsIGrades = new Dictionary<string, List<double>>();
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+                if (!studentsIGrades.ContainsKey(name))
+                {
+                    studentsIGrades.Add(name, new List<double>() { grade });
+                }
+                else
+                {
+                    studentsIGrades[name].Add(grade);
+                }
+            }
+            foreach (var item in studentsIGrades)
+            {
+                if (item.Value.Sum() / item.Value.Count >= 4.5)
+                {
+                    Console.WriteLine($"{item.Key} -> {(item.Value.Sum() / item.Value.Count):f2}");
+                }
+            }
+        }
+    }
+}
