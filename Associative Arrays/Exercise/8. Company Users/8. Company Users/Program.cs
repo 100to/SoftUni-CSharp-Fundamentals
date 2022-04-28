efd6dc94aff44d7f7da9ddf90a42226d3f6a8277
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-04-28 22:40:09 +0300

    Create a program that keeps information about companies and their employees. You will be receiving a company name and an employee's id, until you receive the command "End" command. Add each employee to the given company. Keep in mind that a company cannot have two employees with the same id. When you finish reading the data, order the companies by the name in ascending order. Print the company name and each employee's id in the following format: {companyName} -- {id1} -- {id2} -- {idN}

---
+++ Associative Arrays/Exercise/8. Company Users/8. Company Users/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _8._Company_Users
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var CompanyUsers = new Dictionary<string, List<string>>();
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split(" -> ").ToArray();
+                if (input[0] == "End")
+                {
+                    break;
+                }
+                string company = input[0];
+                string id = input[1];
+                if (!CompanyUsers.ContainsKey(company))
+                {
+                    CompanyUsers.Add(company, new List<string>() { id });
+                }
+                else if (!CompanyUsers[company].Contains(id))
+                {
+                    CompanyUsers[company].Add(id);
+                }
+            }
+            foreach (var item in CompanyUsers)
+            {
+                Console.WriteLine($"{item.Key}");
+                foreach (var item2 in item.Value)
+                {
+                    Console.WriteLine($"-- {item2}");
+                }
+            }
+        }
+    }
+}
