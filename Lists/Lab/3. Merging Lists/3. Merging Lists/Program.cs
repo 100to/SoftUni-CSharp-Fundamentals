64e16f28bd594f8cf9f651e0e9d9ff0c438483b5
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-01 12:38:02 +0300

    You are going to receive two lists of numbers. Create a list that contains the numbers from both of the lists. The first element should be from the first list, the second from the second list, and so on. If the length of the two lists is not equal, just add the remaining elements at the end of the list.

---
+++ Lists/Lab/3. Merging Lists/3. Merging Lists/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Merging_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> numbers2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int bigger = numbers.Count;
+            if (bigger < numbers2.Count)
+            {
+                bigger = numbers2.Count;
+            }
+            for (int i = 0; i < bigger; i++)
+            {
+                if (i < numbers.Count)
+                {
+                    Console.Write($"{numbers[i]} ");
+                }
+                if (i < numbers2.Count)
+                {
+                    Console.Write($"{numbers2[i]} ");
+                }
+            }
+        }
+    }
+}
