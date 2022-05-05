105861a64d147e85c5de2a2fca919574c6f0ec1e
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-05 23:20:51 +0300

    Next, we are going to implement more complicated list commands, extending the previous task. Again, read a list and keep reading commands until you receive "end": •	Contains {number} – check if the list contains the number and if so - print "Yes", otherwise print "No such number". •	PrintEven – print all the even numbers, separated by a space. •	PrintOdd – print all the odd numbers, separated by a space. •	GetSum – print the sum of all the numbers. •	Filter {condition} {number} – print all the numbers that fulfill the given condition. The condition will be either '<', '>', ">=", "<=". After the end command, print the list only if you have made some changes to the original list. Changes are made only from the commands from the previous task.

---
+++ Lists/Lab/7. List Manipulation Advanced/7. List Manipulation Advanced/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7._List_Manipulation_Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            bool wrong = false, changed = false;
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "end" || wrong)
+                {
+                    break;
+                }
+                string[] comm = command.Split();
+                switch (comm[0])
+                {
+                    case "Add":
+                        numbers.Add(int.Parse(comm[1]));
+                        changed = true;
+                        break;
+                    case "Insert":
+                        numbers.Insert(int.Parse(comm[2]), int.Parse(comm[1]));
+                        changed = true;
+                        break;
+                    case "Remove":
+                        numbers.Remove(int.Parse(comm[1]));
+                        changed = true;
+                        break;
+                    case "RemoveAt":
+                        numbers.RemoveAt(int.Parse(comm[1]));
+                        changed = true;
+                        break;
+                    case "Contains":
+                        if (numbers.Contains(int.Parse(comm[1])))
+                        {
+                            Console.WriteLine("Yes");
+                        }
+                        else
+                        {
+                            Console.WriteLine("No such number");
+                        }
+                        break;
+                    case "PrintOdd":
+                        for (int i = 0; i < numbers.Count; i++)
+                        {
+                            if (numbers[i] % 2 != 0)
+                            {
+                                if (i + 1 == numbers.Count)
+                                {
+                                    Console.Write($"{numbers[i]}");
+                                }
+                                else
+                                {
+                                    Console.Write($"{numbers[i]} ");
+                                }
+                            }
+                        }
+                        Console.WriteLine();
+                        break;
+                    case "PrintEven":
+                        for (int i = 0; i < numbers.Count; i++)
+                        {
+                            if (numbers[i] % 2 == 0)
+                            {
+                                if (i + 1 == numbers.Count)
+                                {
+                                    Console.Write($"{numbers[i]}");
+                                }
+                                else
+                                {
+                                    Console.Write($"{numbers[i]} ");
+                                }
+                            }
+                        }
+                        Console.WriteLine();
+                        break;
+                    case "GetSum":
+                        int sum = 0;
+                        for (int i = 0; i < numbers.Count; i++)
+                        {
+                            sum += numbers[i];
+                        }
+                        Console.WriteLine(sum);
+                        break;
+                    case "Filter":
+                        if (comm[1] == "<")
+                        {
+                            for (int i = 0; i < numbers.Count; i++)
+                            {
+                                if (int.Parse(comm[2]) > numbers[i])
+                                {
+                                    if (i + 1 == numbers.Count)
+                                    {
+                                        Console.Write($"{numbers[i]}");
+                                    }
+                                    else
+                                    {
+                                        Console.Write($"{numbers[i]} ");
+                                    }
+                                }
+                            }
+                        }
+                        else if (comm[1] == ">")
+                        {
+                            for (int i = 0; i < numbers.Count; i++)
+                            {
+                                if (int.Parse(comm[2]) < numbers[i])
+                                {
+                                    if (i + 1 == numbers.Count)
+                                    {
+                                        Console.Write($"{numbers[i]}");
+                                    }
+                                    else
+                                    {
+                                        Console.Write($"{numbers[i]} ");
+                                    }
+                                }
+                            }
+                        }
+                        else if (comm[1] == "<=")
+                        {
+                            for (int i = 0; i < numbers.Count; i++)
+                            {
+                                if (int.Parse(comm[2]) >= numbers[i])
+                                {
+                                    if (i + 1 == numbers.Count)
+                                    {
+                                        Console.Write($"{numbers[i]}");
+                                    }
+                                    else
+                                    {
+                                        Console.Write($"{numbers[i]} ");
+                                    }
+                                }
+                            }
+                        }
+                        else if (comm[1] == ">=")
+                        {
+                            for (int i = 0; i < numbers.Count; i++)
+                            {
+                                if (int.Parse(comm[2]) <= numbers[i])
+                                {
+                                    if (i + 1 == numbers.Count)
+                                    {
+                                        Console.Write($"{numbers[i]}");
+                                    }
+                                    else
+                                    {
+                                        Console.Write($"{numbers[i]} ");
+                                    }
+                                }
+                            }
+                        }
+                        Console.WriteLine();
+                        break;
+                    default:
+                        wrong = true;
+                        break;
+                }
+            }
+            if (changed)
+            {
+                Console.WriteLine(string.Join(" ", numbers));
+            }
+        }
+    }
+}
