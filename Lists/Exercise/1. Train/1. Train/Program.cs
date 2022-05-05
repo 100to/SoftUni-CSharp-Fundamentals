3928d17da36d387d47d220683369c5c6482fa200
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-06 00:11:36 +0300

    You On the first line, we will receive a list of wagons (integers). Each integer represents the number of passengers that are currently in each wagon of a passenger train. On the next line, you will receive the max capacity of a wagon represented as a single integer. Until you receive the "end" command, you will be receiving two types of input: •	Add {passengers} – add a wagon to the end of the train with the given number of passengers. •	{passengers} - find a single wagon to fit all the incomming passengers (starting from the first wagon). In the end, print the final state of the train (all the wagons separated by a space).

---
+++ Lists/Exercise/1. Train/1. Train/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int capasity = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "end")
+                {
+                    break;
+                }
+                string[] action = command.Split();
+                if (action[0] == "Add")
+                {
+                    wagons.Add(int.Parse(action[1]));
+                }
+                else
+                {
+                    int put = int.Parse(action[0]);
+                    if (put > capasity || put < 0)
+                    {
+                        break;
+                    }
+                    int i = 0;
+                    while (true)
+                    {
+                        if (i > wagons.Count)
+                        {
+                            break;
+                        }
+                        if (wagons[i] + put <= capasity)
+                        {
+                            wagons[i] += put;
+                            break;
+                        }
+                        i++;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
