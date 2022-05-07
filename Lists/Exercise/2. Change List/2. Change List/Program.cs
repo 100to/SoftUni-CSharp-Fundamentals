0affd0d4e3fd0d79a153182ea5fe2334e3b4d1e4
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-07 20:58:13 +0300

    Create a program, that reads a list of integers from the console and receives commands to manipulate the list. Your program may receive the following commands: •	Delete {element} – delete all elements in the array, which are equal to the given element. •	Insert {element} {position} – insert the element at the given position. You should exit the program when you receive the "end" command. Print all numbers in the array separated by a single whitespace.

---
+++ Lists/Exercise/2. Change List/2. Change List/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Change_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "end")
+                {
+                    break;
+                }
+                string[] action = command.Split();
+                switch (action[0])
+                {
+                    case "Insert":
+                        numbers.Insert(int.Parse(action[2]), int.Parse(action[1]));
+                        break;
+                    case "Delete":
+                        for (int i = 0; i < numbers.Count; i++)
+                        {
+                            if (numbers[i] == int.Parse(action[1]))
+                            {
+                                numbers.Remove(int.Parse(action[1]));
+                                i--;
+                            }
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
