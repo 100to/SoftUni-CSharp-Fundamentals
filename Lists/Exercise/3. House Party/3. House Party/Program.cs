cd79213bfcb23e30c9726ea7aa115960f4682762
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-08 23:29:29 +0300

    Create a program that keeps track of the guests that are going to a house party. On the first line of input, you are going to receive the number of commands that will follow. On the next lines, you are going to receive some of the following:  "{name} is going!" •	You have to add the person if they are not in the guest list already. •	If the person is on the list print to the following to the console: "{name} is already in the list!" "{name} is not going!" •	You have to remove the person if they are in the list. •	If not, print out: "{name} is not in the list!" Finally, print all of the guests, each on a new line.

---
+++ Lists/Exercise/3. House Party/3. House Party/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3._House_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int actions = int.Parse(Console.ReadLine());
+            List<string> guests = new List<string>();
+            for (int i = 0; i < actions; i++)
+            {
+                string sentence = Console.ReadLine();
+                string[] commands = sentence.Split();
+                if (commands[2] == "going!")
+                {
+                    if (guests.Contains(commands[0]))
+                    {
+                        Console.WriteLine($"{commands[0]} is already in the list!");
+                    }
+                    else
+                    {
+                        guests.Add(commands[0]);
+                    }
+                }
+                else
+                {
+                    if (guests.Contains(commands[0]))
+                    {
+                        guests.Remove(commands[0]);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{commands[0]} is not in the list!");
+                    }
+                }
+            }
+            for (int i = 0; i < guests.Count; i++)
+            {
+                Console.WriteLine(guests[i]);
+            }
+        }
+    }
+}
