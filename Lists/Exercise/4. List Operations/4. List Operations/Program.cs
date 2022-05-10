3f048ab8d699145994f4b0ab61927cdb53ed953d
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-10 19:32:35 +0300

    The first input line will hold a list of integers. Until we receive the "End" command, we will be given operations we have to apply to the list. The possible commands are: •	Add {number} – add the given number to the end of the list •	Insert {number} {index} – insert the number at the given index •	Remove {index} – remove the number at the given index •	Shift left {count} – first number becomes last. This has to be repeated the specified number of times •	Shift right {count} – last number becomes first. To be repeated the specified number of times Note: the index given may be outside of the bounds of the array. In that case print: "Invalid index".

---
+++ Lists/Exercise/4. List Operations/4. List Operations/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._List_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "End")
+                {
+                    break;
+                }
+                string[] comm = command.Split();
+                switch (comm[0])
+                {
+                    case "Add":
+                        numbers.Add(int.Parse(comm[1]));
+                        break;
+                    case "Insert":
+                        if (int.Parse(comm[2]) < 0 || int.Parse(comm[2]) > numbers.Count)
+                        {
+                            Console.WriteLine("Invalid index");
+                        }
+                        else
+                        {
+                            numbers.Insert(int.Parse(comm[2]), int.Parse(comm[1]));
+                        }
+                        break;
+                    case "Remove":
+                        if (int.Parse(comm[1]) < 0 || int.Parse(comm[1]) > numbers.Count)
+                        {
+                            Console.WriteLine("Invalid index");
+                        }
+                        else
+                        {
+                            numbers.RemoveAt(int.Parse(comm[1]));
+                        }
+                        break;
+                    case "Shift":
+                        if (comm[1] == "left")
+                        {
+                            for (int i = 0; i < int.Parse(comm[2]) % numbers.Count; i++)
+                            {
+                                numbers.Add(numbers[0]);
+                                numbers.RemoveAt(0);
+                            }
+                        }
+                        else
+                        {
+                            for (int i = 0; i < int.Parse(comm[2]) % numbers.Count; i++)
+                            {
+                                numbers.Insert(0, numbers[numbers.Count - 1]);
+                                numbers.RemoveAt(numbers.Count - 1);
+                            }
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
