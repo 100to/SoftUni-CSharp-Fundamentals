e9cd85ebb2b0f48dc4795b42288931fa4e87250a
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-11 14:35:46 +0300

    Create a program that reads a sequence of numbers and a special bomb number holding a certain power. Your task is to detonate every occurrence of the special bomb number and according to its power the numbers to its left and right. The bomb power refers to how many numbers to the left and right will be removed, no matter their values. Detonations are performed from left to right and all the detonated numbers disappear. Finally, print the sum of the remaining elements in the sequence.

---
+++ Lists/Exercise/5. Bomb Numbers/5. Bomb Numbers/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Bomb_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> bomb = Console.ReadLine().Split().Select(int.Parse).ToList();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == bomb[0])
+                {
+                    for (int j = 0; j < (bomb[1]) + 1; j++)
+                    {
+                        if (numbers.Count == 0)
+                        {
+                            break;
+                        }
+                        if (numbers[0] == bomb[0])
+                        {
+                            numbers.RemoveAt(0);
+                            break;
+                        }
+                        if (bomb[1] > i)
+                        {
+                            numbers.RemoveAt(0);
+                        }
+                        else
+                        {
+                            numbers.RemoveAt(i - bomb[1]);
+                        }
+                    }
+                    for (int j = 0; j < bomb[1]; j++)
+                    {
+                        if (numbers.Count == 0 || i - bomb[1] >= numbers.Count)
+                        {
+                            break;
+                        }
+                        if (bomb[1] > i)
+                        {
+                            numbers.RemoveAt(0);
+                        }
+                        else
+                        {
+                            numbers.RemoveAt(i - bomb[1]);
+                        }
+                    }
+                }
+            }
+            int sum = 0;
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
