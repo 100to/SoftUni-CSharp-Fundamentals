f563532b7ff8738b88af547eef80f58b88cee039
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-14 16:25:05 +0300

    Create a program to append several arrays of numbers one after another. •	Arrays are separated by '|' •	Their Values are separated by spaces (' ', one or several) •	Take all arrays starting from the rightmost and going to the left and place them in a new array in that order

---
+++ Lists/Exercise/7. Append Arrays/7. Append Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string array = Console.ReadLine();
+            string[] firstSeparation = array.Split("|"); // razdelq na nqkolko masiva s razdelenie |
+            List<int> final = new List<int>();
+            for (int i = firstSeparation.Length - 1; i >= 0; i--)
+            {
+                string[] secondSeparation = firstSeparation[i].Split(); // na vsqka chast ot masiva maha po edin space
+                for (int j = 0; j < secondSeparation.Length; j++)
+                {
+                    if (secondSeparation[j] != "")//ako e imalo dvoen space da go premahne i nego
+                    {
+                        final.Add(int.Parse(secondSeparation[j]));
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(" ", final));
+        }
+    }
+}
