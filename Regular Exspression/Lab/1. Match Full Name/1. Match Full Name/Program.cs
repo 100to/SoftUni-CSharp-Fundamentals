68e54584a11e85cd333247e6dcd6c9efd3e81618
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-15 17:37:24 +0300

    Write a C# Program to match full names from a list of names and print them on the console. First, create a regular expression to match a valid full name, according to these conditions: •	A valid full name has the following characteristics: o	It consists of two words. o	Each word starts with a capital letter. o	After the first letter, it only contains lowercase letters afterwards. o	Each of the two words should be at least two letters long. o	The two words are separated by a single space.

---
+++ Regular Exspression/Lab/1. Match Full Name/1. Match Full Name/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _1._Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MatchCollection matches = Regex.Matches(Console.ReadLine(), @"\b[A-Z][a-z]+ [A-Z][a-z]+");
+            foreach (Match item in matches)
+            {
+                Console.Write($"{item.Value} ");
+            }
+        }
+    }
+}
