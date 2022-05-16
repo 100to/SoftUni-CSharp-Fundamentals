3255e395dd7a3a2cb8ffad8090eb97174c40c060
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-16 22:18:34 +0300

    Create a regular expression to match a valid phone number from Sofia. After you find all valid phones, print them on the console, separated by a comma and a space ", ". Compose the Regular Expression A valid number has the following characteristics: •	It starts with "+359" •	Then, it is followed by the area code (always 2) •	After that, it’s followed by the number itself: o	The number consists of 7 digits (separated in two groups of 3 and 4 digits respectively). •	The different parts are separated by either a space or a hyphen ('-'). You can use the following RegEx properties to help with the matching: •	Use quantifiers to match a specific number of digits •	Use a capturing group to make sure the delimiter is only one of the allowed characters (space or hyphen) and not a combination of both (e.g. +359 2-111 111 has mixed delimiters, it is invalid). Use a group backreference to achieve this. •	Add a word boundary at the end of the match to avoid partial matches (the last example on the right-hand side). •	Ensure that before the '+' sign there is either a space or the beginning of the string.

---
+++ Regular Exspression/Lab/2. Match Phone Number/2. Match Phone Number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _2._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MatchCollection matches = Regex.Matches(Console.ReadLine(), @"(\+359 2 [0-9]{3} [0-9]{4})|(\+359\-2\-[0-9]{3}\-[0-9]{4}\b)");
+            foreach (Match item in matches)
+            {
+                Console.Write($"{item.Value} ");
+
+            }
+        }
+    }
+}
