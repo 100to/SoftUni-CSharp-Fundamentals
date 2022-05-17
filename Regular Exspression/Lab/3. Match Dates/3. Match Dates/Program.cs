a7453647376cd0d304d8cf7480e4dea002f13a8c
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-17 20:39:50 +0300

    Write a program, which matches a date in the format "dd{separator}MMM{separator}yyyy". Use named capturing groups in your regular expression. Compose the Regular Expression Every valid date has the following characteristics: •	Always starts with two digits, followed by a separator •	After that, it has one uppercase and two lowercase letters (e.g. Jan, Mar). •	After that, it has a separator and exactly 4 digits (for the year). •	The separator could be either of three things: a period ("."), a hyphen ("-") or a forward slash ("/") •	The separator needs to be the same for the whole date (e.g. 13.03.2016 is valid, 13.03/2016 is NOT). Use a group backreference to check for this.

---
+++ Regular Exspression/Lab/3. Match Dates/3. Match Dates/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _3._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MatchCollection matches = Regex.Matches(Console.ReadLine(), @"\b(?<day>[0-9]{2})(?<separator>[\/|\.\-])(?<month>[A-Z][a-z]{2})(\k<separator>)(?<year>[0-9]{4})\b");
+            foreach (Match item in matches)
+            {
+                Console.WriteLine($"Day: {item.Groups["day"].Value} Month: {item.Groups["month"].Value} Year: {item.Groups["year"].Value}");
+            }
+        }
+    }
+}
