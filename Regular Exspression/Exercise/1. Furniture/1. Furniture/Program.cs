6727ddca074c48eb9d708281e5b8be79f1019709
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-18 22:22:58 +0300

    Create a program to calculate the total cost of different types of furniture. You will be given some lines of input until you receive the line "Purchase". For the line to be valid it should be in the following format: ">>{furniture name}<<{price}!{quantity}" The price can be floating point number or whole number. Store the names of the furniture and the total price. At the end print the each bought furniture on separate line in the format: "Bought furniture: {1st name} {2nd name} …" And on the last line print the following: "Total money spend: {spend money}" formatted to the second decimal point.

---
+++ Regular Exspression/Exercise/1. Furniture/1. Furniture/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _1._Furniture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            List<string> orderedProducts = new List<string>();
+            double sum = 0;
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "Purchase")
+                {
+                    break;
+                }
+                MatchCollection matches = Regex.Matches(input, @"(?<item>[A-Z]+[a-z]*)<<(?<price>[0-9]+\.*[0-9]*)!(?<quantity>[0-9]+)");
+                foreach (Match match in matches)
+                {
+                    if (match.Success)
+                    {
+                        orderedProducts.Add(match.Groups["item"].Value);
+                        sum += double.Parse(match.Groups["price"].Value) * int.Parse(match.Groups["quantity"].Value);
+                    }
+                }
+            }
+            Console.WriteLine("Bought furniture:");
+            foreach (var item in orderedProducts)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Total money spend: {sum:f2}");
+        }
+    }
+}
