8d63cd8125d7294403d97261e5c08ac83e395e9c
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-19 23:30:37 +0300

    Write a program that processes information about a race. On the first line you will be given list of participants separated by ", ". On the next few lines until you receive a line "end of race" you will be given some info which will be some alphanumeric characters. In between them you could have some extra characters which you should ignore. For example: "G!32e%o7r#32g$235@!2e". The letters are the name of the person and the sum of the digits is the distance he ran. So here we have George who ran 29 km. Store the information about the person only if the list of racers contains the name of the person. If you receive the same person more than once just add the distance to his old distance. At the end print the top 3 racers ordered by distance in descending in the format: "1st place: {first racer} 2nd place: {second racer} 3rd place: {third racer}"

---
+++ Regular Exspression/Exercise/2. Race/2. Race/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _2._Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(", ");
+            var info = new Dictionary<string, int>();
+            foreach (var name in names)
+            {
+                if (!info.ContainsKey(name))
+                {
+                    info.Add(name, 0);
+                }
+            }
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end of race")
+                {
+                    break;
+                }
+                MatchCollection matchInfo = Regex.Matches(input, @"(?<distance>\d)|(?<name>[A-Za-z]+)");
+                string name = "";
+                int distance = 0;
+                foreach (Match item in matchInfo)
+                {
+                    foreach (var item2 in item.Groups["distance"].Value)
+                    {
+                        distance += int.Parse(item.Groups["distance"].Value);
+                    }
+                    foreach (var item2 in item.Groups["name"].Value)
+                    {
+                        name += item2;
+                    }
+                }
+                if (info.ContainsKey(name))
+                {
+                    info[name] += distance;
+                }
+            }
+            var sortedInfo = info.OrderByDescending(o => o.Value);
+            int i = 1;
+            foreach (var item in sortedInfo)
+            {
+                if (i == 1)
+                {
+                    Console.WriteLine($"{i}st place: {item.Key}");
+                }
+                else if (i == 2)
+                {
+                    Console.WriteLine($"{i}nd place: {item.Key}");
+                }
+                else if (i == 3)
+                {
+                    Console.WriteLine($"{i}rd place: {item.Key}");
+                }
+                else
+                {
+                    break;
+                }
+                i++;
+            }
+        }
+    }
+}
