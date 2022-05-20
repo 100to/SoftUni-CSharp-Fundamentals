a23d74ae1eb06dc9e83eb04e48fd29789cd5a770
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-20 22:33:19 +0300

    Let's take a break and visit the game bar at SoftUni. It is about time for the people behind the bar to go home and you are the person who has to draw the line and calculate the money from the products that were sold throughout the day. Until you receive a line with text "end of shift" you will be given lines of input. But before processing that line you have to do some validations first. Each valid order should have a customer, product, count and a price: •	Valid customer's name should be surrounded by '%' and must start with a capital letter, followed by lower-case letters •	Valid product contains any word character and must be surrounded by '<' and '>' •	Valid count is an integer, surrounded by '|' •	Valid price is any real number followed by '$' The parts of a valid order should appear in the order given: customer, product, count and a price. Between each part there can be other symbols, except ('|', '$', '%' and '.') For each valid line print on the console: "{customerName}: {product} - {totalPrice}" When you receive "end of shift" print the total amount of money for the day rounded to 2 decimal places in the following format: "Total income: {income}". Input / Constraints •	Strings that you have to process until you receive text "end of shift". Output •	Print all of the valid lines in the format "{customerName}: {product} - {totalPrice}" •	After receiving "end of shift" print the total amount of money for the day rounded to 2 decimal places in the following format: "Total income: {income}" •	Allowed working time / memory: 100ms / 16MB.

---
+++ Regular Exspression/Exercise/3. SoftUni Bar Income/3. SoftUni Bar Income/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _3._SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            double sum = 0;
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "end of shift")
+                {
+                    break;
+                }
+                MatchCollection matches = Regex.Matches(input, @"\%(?<name>[A-Z][a-z]+)\%<(?<item>[A-Z][a-z]+)>\w*\|(?<quantity>[0-9]+)\|\D*(?<price>[0-9]+\.*[0-9]*)\$");
+                foreach (Match match in matches)
+                {
+                    if (match.Success)
+                    {
+                        double priceOfTheOrder = double.Parse(match.Groups["price"].Value) * int.Parse(match.Groups["quantity"].Value);
+                        Console.WriteLine($"{match.Groups["name"].Value}: {match.Groups["item"].Value} - {priceOfTheOrder:f2}");
+                        sum += double.Parse(match.Groups["price"].Value) * int.Parse(match.Groups["quantity"].Value);
+                    }
+                }
+            }
+            Console.WriteLine($"Total income: {sum:f2}");
+        }
+    }
+}
