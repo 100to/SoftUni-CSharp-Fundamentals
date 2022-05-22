fb9feae2a985b3db4c6e87caaf8942e0ca61c1e0
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-22 17:06:24 +0300

    You will be given series of strings until you receive an "end" command. Write a program that reverses strings and prints each pair on separate line in format "{word} = {reversed word}".

---
+++ Strings and Text Processing/Lab/1. Reverse Strings/1. Reverse Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1._Reverse_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+                string reversed = "";
+                for (int i = input.Length - 1; i >= 0; i--)
+                {
+                    reversed += input[i];
+                }
+                Console.WriteLine($"{input} = {reversed}");
+            }
+        }
+    }
+}
