0fc149bd5be82caa6c35262510a129d92afbbda0
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-23 10:22:46 +0300

    Create a Program That Reads an Array of Strings. Each String is Repeated N Times, Where N is the Length of the String. Print the Concatenated String.

---
+++ Strings and Text Processing/Lab/2. Repeat Strings/2. Repeat Strings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Repeat_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split().ToArray();
+            foreach (var word in input)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    Console.Write(word);
+                }
+            }
+        }
+    }
+}
