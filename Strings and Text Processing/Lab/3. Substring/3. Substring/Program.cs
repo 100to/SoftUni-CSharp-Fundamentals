2eeba1954cac114b206fecf3115bcf2e86b02e9a
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-24 22:25:53 +0300

    On the first line you will receive a string. On the second line you will receive a second string. Create a program that removes all of the occurrences of the first string in the second until there is no match. At the end print the remaining string.

---
+++ Strings and Text Processing/Lab/3. Substring/3. Substring/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3._Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string input = Console.ReadLine();
+            while (input.Contains(word))
+            {
+                int index = input.IndexOf(word);
+                input = input.Remove(index, word.Length);
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
