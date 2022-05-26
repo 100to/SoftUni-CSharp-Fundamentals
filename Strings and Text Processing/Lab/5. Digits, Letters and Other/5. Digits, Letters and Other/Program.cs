d50d8eba3d36c1f13d57fe5e8d52f45b7efa19d4
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-26 22:33:14 +0300

    Create a program that receives a single string and on the first line prints all the digits, on the second – all the letters, and on the third – all the other characters. There will always be at least one digit, one letter and one other characters.

---
+++ Strings and Text Processing/Lab/5. Digits, Letters and Other/5. Digits, Letters and Other/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _5._Digits__Letters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] separatedInput = input.ToCharArray();
+            for (int i = 0; i < separatedInput.Length; i++)
+            {
+                if (char.IsDigit(separatedInput[i]))
+                {
+                    Console.Write(separatedInput[i].ToString());
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < separatedInput.Length; i++)
+            {
+                if (char.IsLetter(separatedInput[i]))
+                {
+                    Console.Write(separatedInput[i].ToString());
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < separatedInput.Length; i++)
+            {
+                if (char.IsPunctuation(separatedInput[i]) || char.IsSymbol(separatedInput[i]))
+                {
+                    Console.Write(separatedInput[i].ToString());
+                }
+            }
+        }
+    }
+}
