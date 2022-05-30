11dd213c2e9f954b5ac1163b444defc69e896f01
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-30 23:43:26 +0300

    Create a program that reads the path to a file and subtracts the file name and its extension.

---
+++ Strings and Text Processing/Exercise/3.  Extract File/3.  Extract File/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.__Extract_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] file = Console.ReadLine().Split(@"\").ToArray();
+            int index = file[file.Length - 1].IndexOf(".");
+            string filename = file[file.Length - 1].Substring(0, index);
+            string extension = file[file.Length - 1].Substring(index + 1);
+            Console.WriteLine($"File name: {filename}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
