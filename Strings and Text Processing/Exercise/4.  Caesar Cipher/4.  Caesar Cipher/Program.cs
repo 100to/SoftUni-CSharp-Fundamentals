5dfd7bee08654070645fa2e2c0e079d21b414b90
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-05-31 23:02:45 +0300

    Create a program that returns an encrypted version of the same text. Encrypt the text by shifting each character with three positions forward. For example A would be replaced by D, B would become E, and so on. Print the encrypted text.

---
+++ Strings and Text Processing/Exercise/4.  Caesar Cipher/4.  Caesar Cipher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace _4.__Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            char[] words = input.ToCharArray();
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < words.Length; i++)
+            {
+                int unicode = (int)words[i] + 3;
+                words[i] = (char)unicode;
+                result.Append(words[i]);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
