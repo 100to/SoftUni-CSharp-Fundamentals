8adb6f156e41b9e11f46393d1a5ea4762bc66d5b
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-06-03 08:52:18 +0300

    Create a program that reads a string from the console and replaces any sequence of the same letters with a single corresponding letter.

---
+++ Strings and Text Processing/Exercise/6. Replace Repeating Chars/Replace Repeating Chars/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace Replace_Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            StringBuilder convertedInput = new StringBuilder(input);
+            for (int i = 0; i < convertedInput.Length - 1; i++)
+            {
+                if (convertedInput[i] == convertedInput[i + 1])
+                {
+                    convertedInput.Remove(i, 1);
+                    i--;
+                }
+            }
+            Console.WriteLine(convertedInput);
+        }
+    }
+}
