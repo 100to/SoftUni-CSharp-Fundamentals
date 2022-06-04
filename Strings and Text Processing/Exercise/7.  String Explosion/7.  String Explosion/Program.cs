9402686a42747c050db0dab6c8b35784a0f8dea5
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-06-04 12:55:39 +0300

    Explosions are marked with '>'. Immediately after the mark, there will be an integer, which signifies the strength of the explosion. You should remove x characters (where x is the strength of the explosion), starting after the punch character ('>'). If you find another explosion mark ('>') while you’re deleting characters, you should add the strength to your previous explosion. When all characters are processed, print the string without the deleted characters. You should not delete the explosion character – '>', but you should delete the integers, which represent the strength. Input You will receive single line with the string. Output Print what is left from the string after explosions. Constraints •	You will always receive a strength for the punches •	The path will consist only of letters from the Latin alphabet, integers and the char '>' •	The strength of the punches will be in the interval [0…9]

---
+++ Strings and Text Processing/Exercise/7.  String Explosion/7.  String Explosion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _7.__String_Explosion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int index = 0;
+            int power = 0;
+            while (input.IndexOf(">", index) != -1)
+            {
+                int tempIndex = input.IndexOf(">", index);
+                power += int.Parse(input[tempIndex + 1].ToString());
+                while (power != 0)
+                {
+                    if (input.Length == tempIndex + 1)
+                    {
+                        break;
+                    }
+                    if (input[tempIndex + 1].ToString() == ">")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        input = input.Remove(tempIndex + 1, 1);
+                        power--;
+                    }
+                }
+                index = tempIndex + 1;
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
