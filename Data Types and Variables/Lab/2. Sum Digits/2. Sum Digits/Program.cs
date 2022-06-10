a60f1ee5690141bcf4331d964a135cf53c17e204
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-06-10 21:28:37 +0300

    Create a program that receives a single integer. Your task is to find the sum of its digits. For example: 12345 -> 1+2+3+4+5 = 15

---
+++ Data Types and Variables/Lab/2. Sum Digits/2. Sum Digits/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _2._Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += int.Parse(a[i].ToString());
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
