8b56173093395f74ae2d5f0de1d3990587f064f4
Stoyan Yurukov <stoyanurukov@gmail.com>
2022-06-11 12:47:14 +0300

    Calculate how many courses will be needed to elevate n persons by using an elevator of the capacity of p persons. The input holds two lines: the number of people n and the capacity p of the elevator.

---
+++ Data Types and Variables/Lab/3. Elevator/3. Elevator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _3._Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int persons = int.Parse(Console.ReadLine());
+            int capasity = int.Parse(Console.ReadLine());
+            int sum = 0;
+            sum = (int)Math.Ceiling((double)persons / capasity);
+            Console.WriteLine(sum);
+        }
+    }
+}
